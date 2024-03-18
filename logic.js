199b9890220f67a7a5d19db7ae846a460a733eaf
JAK <jonathanakidd@gmail.com>
2024-03-17 22:02:09 -0400

    Add function for grid squares, event listener for coloring, and function to clear and create new grid.

--- logic.js
+++ logic.js
@@ -1,11 +1,33 @@
-let numberOfSquares = 16**2;
-let grid = document.querySelector('.grid');
-
+let grid = document.querySelector('.grid'); 
 function createGrid(numberOfSquares) {
-    for (let i = 0; i < numberOfSquares; i++){
+    grid.innerHTML = '';
+    let  squares = numberOfSquares**2;
+    grid.style.height = grid.offsetWidth + 'px'
+    let setWidth  =  100/numberOfSquares;
+    for (let i = 0; i < squares; i++){
         let div = document.createElement('div');
-        div.innerHTML = 'This is a div';
         grid.appendChild(div);
-    }
-}
+        div.style.display = 'flex';
+        div.style.flexBasis = 'calc('+setWidth +'% - 2px';
+        div.style.height = 'calc('+setWidth +'% - 2px'
+        div.addEventListener('mouseover', (event) => {
+            div.style.backgroundColor = 'yellow'
+        });
 
+    }
+};
+
+
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', (event) => {
+    let numberOfSquares = null;
+    while ((numberOfSquares >= 1 && numberOfSquares <= 100) == false) {
+        numberOfSquares = prompt("Please input a number of 1 through 100.")
+        if (numberOfSquares > 0 && numberOfSquares < 101) {
+            createGrid(numberOfSquares);
+        }
+        else {
+            let numberOfSquares = prompt("Please input a number of 1 through 100.")
+        }
+    }
+});
